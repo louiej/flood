a869d4ad08d0b553e4efc3b4634438a6c2924276
Louie Riños <louierinos@gmail.com>
2015-02-23 17:14:55 +0800

    Project Start

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name floodidsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the floodidsApp
+ */
+angular.module('floodidsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name floodidsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the floodidsApp
+ */
+angular.module('floodidsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karmahdsadsa'
+
+    ];
+
+    $scope.temp = 'louie'
+  });
